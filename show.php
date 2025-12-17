4af326f543b8ac4641f274b31faff0a88a40cdcd
alyabarihussein-code <alyabarihussein@gmail.com>
2025-12-17 18:16:52 +0300

    عرض البيانات

---
+++ show.php
@@ -0,0 +1,16 @@
+<?php
+include 'connect.php'; 
+
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "ID: " . $row["id"] . " - Username: " . $row["username"] . " - Email: " . $row["email"] . "<br>";
+    }
+} else {
+    echo "لا توجد بيانات";
+}
+
+$conn->close();
+?>
